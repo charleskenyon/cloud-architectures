57b82dfbec3b4f701bfe9d0a0c36db687209dd0e
Rory Kenyon <rory.kenyon01@gmail.com>
2026-03-09 07:44:08 +0000

    debug buildspec

--- architectures/blue-green-deployment/terraform/main.tf
+++ architectures/blue-green-deployment/terraform/main.tf
@@ -115,3 +115,23 @@ resource "aws_codebuild_project" "codebuild_build" {
     buildspec = "architectures/blue-green-deployment/lambda/buildspec.yml"
   }
 }
+
+
+
+
+# resource "aws_codedeploy_app" "codedeploy_app" {
+#   name = "appexample-dev-codedeploy-us-east-1"
+#   compute_platform = "Server"
+# }
+
+# resource "aws_codedeploy_deployment_group" "deployment_group" {
+#   app_name              = aws_codedeploy_app.codedeploy_app.name
+#   deployment_group_name = "example-group"
+#   service_role_arn      = aws_iam_role.codedeploy_role.arn
+#   deployment_config_name = "CodeDeployDefault.AllAtOnce"
+#   autoscaling_groups = [aws_autoscaling_group.autoscaling_group.name]
+#   auto_rollback_configuration {
+#     enabled = true
+#     events  = ["DEPLOYMENT_FAILURE"]
+#   }
+# }
